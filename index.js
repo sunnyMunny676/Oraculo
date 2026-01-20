3e48b1479ebb62d93ce2e77f4ccff4bd1544b0ca
sunnyMunny676 <chaossuunny@gmail.com>
2026-01-20 16:29:25 -0600

    index.js
    Cerebro- memoria

---
+++ index.js
@@ -0,0 +1,28 @@
+const { default: makeWASocket, useMultiFileAuthState } = require('@whiskeysockets/baileys')
+const qrcode = require('qrcode-terminal')
+
+async function conectarWhatsApp() {
+    const { state, saveCreds } = await useMultiFileAuthState('sesion_oraculo')
+    
+    const sock = makeWASocket({
+        auth: state,
+        printQRInTerminal: true
+    })
+
+    sock.ev.on('creds.update', saveCreds)
+
+    sock.ev.on('messages.upsert', async m => {
+        const msg = m.messages[0]
+        if (!msg.message || msg.key.fromMe) return
+        
+        const texto = msg.message.conversation || msg.message.extendedTextMessage?.text
+        
+        if (texto === '.hola') {
+            await sock.sendMessage(msg.key.remoteJid, { text: '¡Hola! Soy Oráculo, tu bot personal.' })
+        }
+    })
+
+    console.log("Bot encendido y esperando mensajes...")
+}
+
+conectarWhatsApp()
